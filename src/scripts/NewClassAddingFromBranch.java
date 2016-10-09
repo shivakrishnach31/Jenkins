f1e412d36d547ba242c0c2bedabea1605770bab6
shivakrishnach31 <shiva.ch31@gmail.com>
2016-10-09 18:28:04 +0530

    added new class

---
+++ src/scripts/NewClassAddingFromBranch.java
@@ -0,0 +1,10 @@
+package scripts;
+
+public class NewClassAddingFromBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello new class from branch");
+	}
+
+}
